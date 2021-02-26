a279b10e625f9f0546b52dbb16a83f5d590fade3
rouxxi <laskar.gwenvael@gmail.com>
2021-02-26 10:39:53 +0100

    for deploy

--- src/component/CrewMembers/CrewMembers.jsx
+++ src/component/CrewMembers/CrewMembers.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import './CrewMembers.css'
 
 function CrewMembers() {
+const [members, setMembers] = React.useState([]);
+const {REACT_APP_BACKURL} = process.env;
 
+useEffect(()=> {
+ axios.get(`${REACT_APP_BACKURL}crewmembers`).then((res)=>setMembers(res.data))
+}, [members])
     return (
-<div>CrewMembers</div>
+<div> <h2 className='H4crew'>The crew</h2>
+<ul> 
+{members && members.map((bro)=> {
+    return ( 
+    <li>
+        {bro.name}
+    </li>
+        )
+})
+}
+</ul>
+
+</div>
     )
 }
 
--- src/component/SubscribeForm/SubscribeForm.jsx
+++ src/component/SubscribeForm/SubscribeForm.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
+import axios from 'axios';
 
 function SubscribeForm() {
+    const [message, setMessage] = React.useState("")
     const [name, setName] = React.useState("")
+    const {REACT_APP_BACKURL} = process.env;
+    
     const handleChange = (e) => {
         setName(e.target.value)
     }
     const onSubmit = (event) => {
      event.preventDefault();
-
+     if(name.length>0) {
+        axios.post(`${REACT_APP_BACKURL}crewmembers`, {
+            name:name
+        }).then((res)=>
+        {
+            if(res.data.msg){
+                setMessage(res.data.msg)
+            } else {
+                setMessage('');
+                setName('');
+            }
+        } )
+     }else {
+        setMessage('Enter the name of somone plz!')
+     }
+     
+    
     }
+
+
     return (
+        <div>
         <form>
             <label htmlFor="">Name:&nbsp;
             <input type="text" value={name} onChange={handleChange}/>
@@ -17,6 +40,8 @@ function SubscribeForm() {
             &nbsp;
             <input type="submit" value='Send' onClick={onSubmit} />
         </form>
+        {message && <div>{message}</div>}
+        </div>
     )
 }
 
